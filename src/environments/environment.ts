102652c13293d9760ccc4ec38bf1982d78f36640
peltastic <onosogapelumi@gmail.com>
2025-09-19 15:26:22 +0100

    fix: handled transfer and swap modal error handling and added readme

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   exchangeRateApiUrl: 'https://api.exchangeratesapi.io/v1',
-  exchangerateApiKey: '35a527b8902d6c9b5833767000993601',
+  exchangerateApiKey: '',
   appName: 'Cross Pay Dev',
   version: '1.0.0-dev',
 };
